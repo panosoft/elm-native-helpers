19b4e6514d01381fcab2ab111d5e8cb643e936af
Charles Scalfani <charles@panosoft.com>
2016-09-02 11:15:36 -0700

    Added more functions and documentation, minor cleanup

--- cmd.js
+++ cmd.js
@@ -1,8 +1,20 @@
+// E must be passed from the elm native module since required modules won't live in the same scope as elm
+///
+// It must have at least the following:
+//
+// const E = {
+// 	Scheduler: {
+// 		nativeBinding: _elm_lang$core$Native_Scheduler.nativeBinding,
+// 		succeed:  _elm_lang$core$Native_Scheduler.succeed,
+// 		rawSpawn: _elm_lang$core$Native_Scheduler.rawSpawn
+// 	}
+// };
+
 module.exports = E => {
 	const id = x => x;
 	const cmdCallback0 = (settings, err) => E.Scheduler.rawSpawn(err ? settings.onError(err) : settings.onSuccess());
 	const cmdCallback1 = (settings, err, result1) => E.Scheduler.rawSpawn(err ? settings.onError(err) : settings.onSuccess(result1));
-	const cmdCallback2 = (settings, err, result1, result2) => E.Scheduler.rawSpawn(err ? settings.onError(err) : A2(settings.onSuccess, result1, result2));
+	const cmdCallback2 = (settings, err, result1, result2) => E.Scheduler.rawSpawn(err ? settings.onError(err) : E.A2(settings.onSuccess, result1, result2));
 	// Cmd Helpers
 	const cmdHelper = f => {
 		return E.Scheduler.nativeBinding(callback => {
@@ -37,68 +49,111 @@ module.exports = E => {
 	const cmdCall1_1 = (f, postProcess) => (settings, p1) => {
 		postProcess = postProcess || postProcessNop;
 		return cmdHelper(callback => {
-			f(p1, (err, result1) => cmdCallback1.apply(null, [settings, err].concat(postProcess([result1]))));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
-		});
-	};
-	const cmdCall3_1 = (f, unwrapper, postProcess) => (settings, p1, p2, p3) => {
-		unwrapper = unwrapper || unwrapNop;
-		postProcess = postProcess || postProcessNop;
-		return cmdHelper(callback => {
-			f.apply(null, append(unwrapper(0, [p1, p2, p3]), (err, result1) => cmdCallback1.apply(null, [settings, err].concat(postProcess([result1])))));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
+			f(p1, (err, result1) => cmdCallback1(settings, err, postProcess(result1)));
+			callback(E.Scheduler.succeed(null));
 		});
 	};
 	const cmdCall1_0 = (f, unwrapper) => (settings, p1) => {
 		unwrapper = unwrapper || unwrapNop;
 		return cmdHelper(callback => {
 			f.apply(null, append(unwrapper(0, [p1]), err => cmdCallback0(settings, err)));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
+			callback(E.Scheduler.succeed(null));
 		});
 	};
 	const cmdCall2_0 = (f, unwrapper) => (settings, p1, p2) => {
 		unwrapper = unwrapper || unwrapNop;
 		return cmdHelper(callback => {
 			f.apply(null, append(unwrapper(0, [p1, p2]), err => cmdCallback0(settings, err)));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
+			callback(E.Scheduler.succeed(null));
+		});
+	};
+	const cmdCall2_1 = (f, unwrapper, postProcess) => (settings, p1, p2) => {
+		unwrapper = unwrapper || unwrapNop;
+		postProcess = postProcess || postProcessNop;
+		return cmdHelper(callback => {
+			f.apply(null, append(unwrapper(0, [p1, p2]), (err, result1) => cmdCallback1(settings, err, postProcess(result1))));
+			callback(E.Scheduler.succeed(null));
 		});
 	};
 	const cmdCall3_0 = (f, unwrapper) => (settings, p1, p2, p3) => {
 		unwrapper = unwrapper || unwrapNop;
 		return cmdHelper(callback => {
 			f.apply(null, append(unwrapper(0, [p1, p2, p3]), err => cmdCallback0(settings, err)));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
+			callback(E.Scheduler.succeed(null));
+		});
+	};
+	const cmdCall3_1 = (f, unwrapper, postProcess) => (settings, p1, p2, p3) => {
+		unwrapper = unwrapper || unwrapNop;
+		postProcess = postProcess || postProcessNop;
+		return cmdHelper(callback => {
+			f.apply(null, append(unwrapper(0, [p1, p2, p3]), (err, result1) => cmdCallback1(settings, err, postProcess(result1))));
+			callback(E.Scheduler.succeed(null));
+		});
+	};
+	const cmdCall3_2 = (f, unwrapper, postProcess1, postProcess2) => (settings, p1, p2, p3) => {
+		unwrapper = unwrapper || unwrapNop;
+		postProcess1 = postProcess1 || postProcessNop;
+		postProcess2 = postProcess2 || postProcessNop;
+		return cmdHelper(callback => {
+			f.apply(null, append(unwrapper(0, [p1, p2, p3]), (err, result1, result2) => cmdCallback2(settings, err, postProcess1(result1), postProcess2(result2))));
+			callback(E.Scheduler.succeed(null));
 		});
 	};
 	const cmdCall4_1 = (f, unwrapper, postProcess) => (settings, p1, p2, p3, p4) => {
 		unwrapper = unwrapper || unwrapNop;
 		postProcess = postProcess || postProcessNop;
 		return cmdHelper(callback => {
-			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4]), (err, result1) => cmdCallback1.apply(null, [settings, err].concat(postProcess([result1])))));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
+			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4]), (err, result1) => cmdCallback1(settings, err, postProcess(result1))));
+			callback(E.Scheduler.succeed(null));
+		});
+	};
+	const cmdCall4_2 = (f, unwrapper, postProcess1, postProcess2) => (settings, p1, p2, p3, p4) => {
+		unwrapper = unwrapper || unwrapNop;
+		postProcess1 = postProcess1 || postProcessNop;
+		postProcess2 = postProcess2 || postProcessNop;
+		return cmdHelper(callback => {
+			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4]), (err, result1, result2) => cmdCallback2(settings, err, postProcess1(result1), postProcess2(result2))));
+			callback(E.Scheduler.succeed(null));
 		});
 	};
 	const cmdCall5_0_opts = (f, unwrap, names) => (settings, p1, p2, p3, p4, p5) => {
 		const args = [p1, p2, p3, p4, p5];
 		return cmdHelper(callback => {
 			f.apply(null, unwrap(names.length, buildArgs(args, names, err => cmdCallback0(settings, err))));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
+			callback(E.Scheduler.succeed(null));
 		});
 	};
 	const cmdCall5_1 = (f, unwrapper, postProcess) => (settings, p1, p2, p3, p4, p5) => {
 		unwrapper = unwrapper || unwrapNop;
 		postProcess = postProcess || postProcessNop;
 		return cmdHelper(callback => {
-			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4, p5]), (err, result1) => cmdCallback1.apply(null, [settings, err].concat(postProcess([result1])))));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
+			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4, p5]), (err, result1) => cmdCallback1(settings, err, postProcess(result1))));
+			callback(E.Scheduler.succeed(null));
 		});
 	};
 	const cmdCall6_1 = (f, unwrapper, postProcess) => (settings, p1, p2, p3, p4, p5, p6) => {
 		unwrapper = unwrapper || unwrapNop;
 		postProcess = postProcess || postProcessNop;
 		return cmdHelper(callback => {
-			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4, p5, p6]), (err, result1) => cmdCallback1.apply(null, [settings, err].concat(postProcess([result1])))));
-			callback(E.Scheduler.succeed(E.Maybe.Nothing));
+			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4, p5, p6]), (err, result1) => cmdCallback1(settings, err, postProcess(result1))));
+			callback(E.Scheduler.succeed(null));
+		});
+	};
+	const cmdCall7_1 = (f, unwrapper, postProcess) => (settings, p1, p2, p3, p4, p5, p6, p7) => {
+		unwrapper = unwrapper || unwrapNop;
+		postProcess = postProcess || postProcessNop;
+		return cmdHelper(callback => {
+			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4, p5, p6, p7]), (err, result1) => cmdCallback1(settings, err, postProcess(result1))));
+			callback(E.Scheduler.succeed(null));
+		});
+	};
+	const cmdCall7_2 = (f, unwrapper, postProcess1, postProcess2) => (settings, p1, p2, p3, p4, p5, p6, p7) => {
+		unwrapper = unwrapper || unwrapNop;
+		postProcess1 = postProcess1 || postProcessNop;
+		postProcess2 = postProcess2 || postProcessNop;
+		return cmdHelper(callback => {
+			f.apply(null, append(unwrapper(0, [p1, p2, p3, p4, p5, p6, p7]), (err, result1, result2) => cmdCallback2(settings, err, postProcess1(result1), postProcess2(result2))));
+			callback(E.Scheduler.succeed(null));
 		});
 	};
 
@@ -106,11 +161,16 @@ module.exports = E => {
 		unwrap,
 		cmdCall1_0,
 		cmdCall2_0,
+		cmdCall2_1,
 		cmdCall3_0,
 		cmdCall3_1,
+		cmdCall3_2,
 		cmdCall4_1,
+		cmdCall4_2,
 		cmdCall5_1,
 		cmdCall5_0_opts,
-		cmdCall6_1
+		cmdCall6_1,
+		cmdCall7_1,
+		cmdCall7_2
 	};
 };
--- readme.md
+++ readme.md
@@ -11,7 +11,56 @@ npm install @panosoft/elm-native-helpers
 ## Usage
 
 ```js
-const cmd = require('@panosoft/elm-native-helpers/cmd');
+const E = {
+	Scheduler: {
+		nativeBinding: _elm_lang$core$Native_Scheduler.nativeBinding,
+		succeed:  _elm_lang$core$Native_Scheduler.succeed,
+		rawSpawn: _elm_lang$core$Native_Scheduler.rawSpawn
+	}
+};
+const cmd = require('@panosoft/elm-native-helpers/cmd')(E);
+```
+## Scoping
+
+Normally, Elm code is all in the global space. But when run in Node, the Elm code gets brought in via the `require` function. This creates a ***private*** scope for Elm.
+
+Since this package calls some Elm global functions, we need to pass them in from our Native code. That's why `E` is passed in.
+
+Here's an example of what Node program might look like that `require`s our Elm program.
+
+```js
+// compile Main.elm with:
+//		elm make Main.elm --output main.js
+
+// load Elm module
+const elm = require('./main.js');
+
+// get Elm ports
+const ports = elm.Main.worker().ports;
+
+// keep our app alive until we get an exitCode from Elm or SIGINT or SIGTERM (see below)
+setInterval(id => id, 86400);
+
+ports.node.subscribe(exitCode => {
+	console.log('exit code from Elm:', exitCode);
+	process.exit(exitCode);
+});
+
+process.on('uncaughtException', err => {
+	console.log(`Uncaught exception:`, {err: err});
+	process.exit(1);
+});
+
+process.on('SIGINT', () => {
+	console.log(`SIGINT received.`);
+	process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+	console.log(`SIGTERM received.`);
+	process.exit(0);
+});
+
 ```
 
 ## Warning
